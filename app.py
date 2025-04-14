a8fe751cdebb344a47d447be0181cc7e465d00b9
kowalski <aleksikondratiuk@gmail.com>
2025-04-15 00:59:29 +0300

    Login page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "This is the fakedrive"
+def login():
+    return render_template("login.html")
