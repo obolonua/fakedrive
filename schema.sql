0ec6e4905cad419f680e9bb0b185d10e2febe420
obolonua <obolonua>
2025-05-04 20:06:30 +0300

    comments table

--- schema.sql
+++ schema.sql
@@ -10,4 +10,11 @@ CREATE TABLE files (
     filepath TEXT,
     owner_id INTEGER REFERENCES users,
     public INTEGER
-)
+);
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY,
+    file_id INTEGER REFERENCES files(id),
+    user_id INTEGER REFERENCES users(id),
+    message TEXT
+);
